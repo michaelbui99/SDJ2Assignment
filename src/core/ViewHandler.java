7f92c4364b22123916944a081468fbf964b34eff
Michael Bui <michaelbui99@hotmail.com>
2021-02-22 14:01:30 +0100

    fixed weird class

--- src/core/ViewHandler.java
+++ src/core/ViewHandler.java
@@ -1,14 +1,5 @@
 package core;
 
-import view.mainView.MainViewController;
-import view.controlView.ControlViewController;
-
-public class ViwHandler {
-
-	private ViewModelFactory viewModelFactory;
-
-	private MainViewController mainViewController;
-
-	private ControlViewController controlViewController;
-
+public class ViewHandler
+{
 }
