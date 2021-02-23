6e842adefaacbf4b92d698c062ab597d1df6cefc
Kutaiba-Nezar-Kashmar <kkashmar94.kk@gmail.com>
2021-02-23 10:47:56 +0100

    App + StartApp

--- src/StartApp.java
+++ src/StartApp.java
@@ -2,7 +2,7 @@ import javafx.application.Application;
 
 public class  StartApp {
 
-	public void main(String[] args)
+	public static void main(String[] args)
 	{
 		Application.launch(App.class);
 	}
