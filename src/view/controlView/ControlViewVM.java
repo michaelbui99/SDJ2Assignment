eebf2836d416c18d5be4515bde36fd827cff947b
Michael Bui <michaelbui99@hotmail.com>
2021-02-23 08:24:33 +0100

    Added constructors to ViewModels

--- src/view/controlView/ControlViewVM.java
+++ src/view/controlView/ControlViewVM.java
@@ -1,5 +1,17 @@
 package view.controlView;
 
+import model.heater.HeaterModel;
+import model.temperature.TemperatureModel;
+
 public class ControlViewVM {
+
+  private TemperatureModel temperatureModel;
+  private HeaterModel heaterModel;
+
+  public ControlViewVM(TemperatureModel temperatureModel, HeaterModel heaterModel)
+  {
+    this.temperatureModel = temperatureModel;
+    this.heaterModel = heaterModel;
+  }
 
 }
--- src/view/mainView/MainViewVM.java
+++ src/view/mainView/MainViewVM.java
@@ -1,5 +1,16 @@
 package view.mainView;
 
+import model.heater.HeaterModel;
+import model.temperature.TemperatureModel;
+
 public class MainViewVM {
 
+  private TemperatureModel temperatureModel;
+  private HeaterModel heaterModel;
+
+  public MainViewVM(TemperatureModel temperatureModel, HeaterModel heaterModel)
+  {
+    this.temperatureModel = temperatureModel;
+    this.heaterModel = heaterModel;
+  }
 }
