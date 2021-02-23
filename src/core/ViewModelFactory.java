f4b95a7bd0b368ab899e2c09e75fe85ea52abdca
Michael Bui <michaelbui99@hotmail.com>
2021-02-23 08:27:51 +0100

    Added vmf methods

--- src/core/ViewModelFactory.java
+++ src/core/ViewModelFactory.java
@@ -8,6 +8,20 @@ public class ViewModelFactory {
 	private ControlViewVM controlViewVM;
 	private MainViewVM mainViewVM;
 
+	public ViewModelFactory(ModelFactory mf)
+	{
+		controlViewVM = new ControlViewVM(mf.getTemperatureModel(),
+				mf.getHeaterModel());
+		mainViewVM = new MainViewVM(mf.getTemperatureModel(), mf.getHeaterModel());
+	}
 
+	public ControlViewVM getControlViewVM()
+	{
+		return controlViewVM;
+	}
 
+	public MainViewVM getMainViewVM()
+	{
+		return mainViewVM;
+	}
 }
